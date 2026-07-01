7ac6eea1f8cc22df3e0952d5b180bc1ce6080b38
Islamux <fathi733@gmail.com>
2026-07-01 09:26:15 +0300

    feat: add useDataSync hook with localStorage change listener

--- src/hooks/useDataSync.ts
+++ src/hooks/useDataSync.ts
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import { syncBackend } from '../utils/syncBackend';
+import { localStorageBackend } from '../utils/localStorage';
 
 export function useDataSync() {
   const [syncPending, setSyncPending] = useState(syncBackend.isSyncPending());
 
   useEffect(() => {
     syncBackend.initFromServer();
+
+    localStorageBackend.onChange(() => syncBackend.notifyChange());
 
     const unsub = syncBackend.onChange(() => {
       setSyncPending(syncBackend.isSyncPending());
